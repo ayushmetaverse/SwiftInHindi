ad21f080b34eda978b493260476b2df3856fc1b7
ayushmetaverse <ayushmt3@gmail.com>
2024-05-03 23:22:23 +0530

    Update class-2.swift

--- Higher Order Func/class-2.swift
+++ Higher Order Func/class-2.swift
@@ -6,5 +6,11 @@ func reduceFunc (){
     let list = [23,43,2,5,6,22,584]
 
     print(list.reduce(0, +))
+    print(list.reduce(1, *))
+
+    let words = ["Hello World" , " " , "Ayush" ,  " "]
+
 
 }
+
+reduceFunc()
